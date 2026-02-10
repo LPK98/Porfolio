0cd47fa9b53e89410f69ba9d6910ac86c8edfbe6
Lal Pushpakumaran <161225569+LPK98@users.noreply.github.com>
2026-02-10 10:19:18 +0530

    profile picture padding adjustment

--- luxury-portfolio/src/data/stats.js
+++ luxury-portfolio/src/data/stats.js
@@ -2,5 +2,5 @@ export const stats = [
   { value: "20+", label: "Projects Delivered" },
   { value: "2", label: "Years Experience" },
   { value: "10+", label: "Happy Clients" },
-  { value: "10%", label: "Client Satisfaction" },
+  { value: "80%", label: "Client Satisfaction" },
 ];
